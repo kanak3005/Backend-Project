b0ede0195c27cbeb76397aeacf612135858b6b4e
Kanak <kanak732080@gmail.com>
2026-07-31 13:21:03 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 // require('dotenv').config({path: './env'})
-import dns from 'dns';
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants.js";
+import dns from 'dns'; // DNS - Domain Name System Jab bhi Node.js kisi website ya MongoDB Atlas se connect karta hai, pehle DNS us domain ka IP address find karta hai.
+dns.setServers(['8.8.8.8', '8.8.4.4']);//"Apne default DNS ki jagah Google ke DNS servers use karo."
+import dotenv from "dotenv"; //.env file se variables read karna.
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-dotenv.config({ path: "./.env" })
+dotenv.config({ path: "./.env" }) //.env file ko load karta hai.
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERR:", error);
+        throw error;
+    })
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port: ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!!", err)
+})
 
 
 
