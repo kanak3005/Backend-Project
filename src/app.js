739731ec6e047582754a6d290059cdee1a7d70ed
Kanak <kanak732080@gmail.com>
2026-09-15 07:09:42 +0530

    add healthcheck route and controller for server status monitoring

--- src/app.js
+++ src/app.js
@@ -21,12 +21,26 @@ app.use(cookieParser()) // Browser se aane wali raw cookie string ko parse karke
 // Session server ke paas hota hai.
 
 // routes
-import { router as userRouter } from "./routes/user.routes.js"
-
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration 
 app.use("/api/v1/users", userRouter) ///api/v1/users se start hone wali requests ko userRouter ke paas bhejo.
-
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 //  /user -> ke baad jo bhi methods likhe jayenge vo sab routes.js m likhenge
 // http://localhost:8000/api/v1/users - jaise hi humne /users likha aab saara control userRouter ke pass (user.routes.js ) chla jayega vha /register - > ke baad registerUser method chlega
 // http://localhost:8000/api/v1users/register
